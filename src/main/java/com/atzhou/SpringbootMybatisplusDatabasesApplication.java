d67e68bb4e6d5dc9c8f0431bc0fb7325dda8edb8
@atzhou624029642 <624029642@qq.com>
2022-07-19 11:50:09 +0800

    Initial commit

---
+++ src/main/java/com/atzhou/SpringbootMybatisplusDatabasesApplication.java
@@ -0,0 +1,13 @@
+package com.atzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusDatabasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusDatabasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atzhou/SpringbootMybatisplusDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atzhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisplusDatabasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
